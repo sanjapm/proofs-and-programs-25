f2f26aa2a3d8e44364e28adea0baf3c4b5ec8d06
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-12-31 15:47:07 +0530

    tweaking for docs

--- PfsProgs25/Unit00/Egs.lean
+++ PfsProgs25/Unit00/Egs.lean
@@ -4,3 +4,11 @@ import Mathlib
 
 We see our first examples of programs and proofs in Lean. A major goal is to illustrate *functional programming*, where we use recursions for loops and avoid mutable variables.
 -/
+
+def sumToN (n: Nat) : Nat := Id.run do
+  let mut sum := 0
+  for i in [1:n+1] do
+    sum := sum + i
+  return sum
+
+#eval sumToN 10
--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ set -e
 ./mk_imports.sh
 lake build
 cd docbuild
-rm -rf .lake/build/doc
+# rm -rf .lake/build/doc
 lake build PfsProgs25:docs
 rsync -avz .lake/build/doc/ gadgil@math.iisc.ac.in:~/public_html/PfsProgs25doc
 cd ..
