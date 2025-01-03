7e3720a63ff3d2f00897b573b79292fc9ff3cdef
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-01-03 10:51:38 +0530

    post lecture

--- PfsProgs25/Unit00/Egs.lean
+++ PfsProgs25/Unit00/Egs.lean
@@ -4,3 +4,21 @@ import Mathlib
 
 We see our first examples of programs and proofs in Lean. A major goal is to illustrate *functional programming*, where we use recursions for loops and avoid mutable variables.
 -/
+def sumToN (n: Nat) : Nat :=
+  match n with
+  | 0 => 0
+  | m + 1 =>
+    (sumToN m) + (m + 1)
+
+#eval sumToN 10 -- 55
+
+theorem sumToN_eq (n: Nat) : 2 * sumToN n = n * (n + 1) := by
+  induction n with
+  | zero => rfl
+  | succ m ih =>
+    unfold sumToN
+    rw [left_distrib, ih]
+    ring
+
+partial def silly (n: Nat) : Nat :=
+  silly (n + 1)
