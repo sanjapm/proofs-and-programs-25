26aef96184a63b0fee09857f7d4d1cf5bc6cb70e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-01-30 06:27:51 +0000

    dependency, demo update

--- PfsProgs25/Misc/AideDemo.lean
+++ PfsProgs25/Misc/AideDemo.lean
@@ -1,6 +1,8 @@
 import Mathlib
 import LeanAideTools
 
+set_option leanaide.url  "http://10.134.10.33:7654"
+
 #theorem "There are infinitely many natural numbers"
 
 #def "A number is defined to be cube-free if it is not divisible by the cube of a prime number"
