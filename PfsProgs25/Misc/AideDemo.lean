943ce8928a9a6007cd7c2b3c924683031a80c59e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-02-02 11:30:53 +0000

    manifest updated, prove example

--- PfsProgs25/Misc/AideDemo.lean
+++ PfsProgs25/Misc/AideDemo.lean
@@ -6,3 +6,5 @@ set_option leanaide.url  "http://10.134.13.102:7654"
 #theorem "There are infinitely many natural numbers"
 
 #def "A number is defined to be cube-free if it is not divisible by the cube of a prime number"
+
+#prove "The product of two successive natural numbers is even"
