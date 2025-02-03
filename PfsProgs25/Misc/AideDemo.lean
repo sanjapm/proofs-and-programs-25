1c793a902385cb37fcaeb0dcd0c2af60825f751d
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-02-03 10:06:30 +0000

    update, added example

--- PfsProgs25/Misc/AideDemo.lean
+++ PfsProgs25/Misc/AideDemo.lean
@@ -1,10 +1,14 @@
 import Mathlib
 import LeanAideTools
 
-set_option leanaide.url  "http://10.134.13.102:7654"
+-- set_option leanaide.url  "http://10.134.13.102:7654"
 
 #theorem "There are infinitely many natural numbers"
 
 #def "A number is defined to be cube-free if it is not divisible by the cube of a prime number"
 
 #prove "The product of two successive natural numbers is even"
+
+theorem eg₁ (n: Nat) : 2 ∣ n * (n + 1) := by
+  -- #prove
+  sorry
